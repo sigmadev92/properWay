be87d9bf4c5facc74ed05ef130be425883410dfb
sigmadev92 <devanshofficial2000@gmail.com>
2024-07-06 16:02:02 +0530

    initial

---
+++ index.js
@@ -0,0 +1,3 @@
+"At the forefront of our lineup for top Java IDEs stands IntelliJ IDEA. With over 33% of the market share, a stellar user rating of 4.3 and a whopping 89% user satisfaction, it's the most adopted Java IDE."
+
+"IntelliJ IDEA has unique resources such as a version control system, frameworks and multi-language support. Robust code analysis, seamless integration with popular frameworks such as Spring Boot, and intelligent coding assistance make it popular among developers. Hire Java developers with knowledge of IntelliJ IDEA to build high-quality Java applications with higher efficiency and precision. "
